208a9aee5238f96f5d993e0d0d24865c5297cb8f
Scott Carlson <scarlson@inbox.com>
2013-01-09 10:01:17 -0700

    more unicorn work

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,35 +5,9 @@ worker_processes 3
 
 # Load your app into the master before forking
 # workers for super-fast worker spawn times
-preload_app true
+preload_app false
 
 # Immediately restart any workers that
 # haven't responded within 30 seconds
 timeout 30
 
-
-after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
-end
-
-before_fork do |server, worker|
-  ##
-  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
-  # immediately start loading up a new version of itself (loaded with a new
-  # version of our app). When this new Unicorn is completely loaded
-  # it will begin spawning workers. The first worker spawned will check to
-  # see if an .oldbin pidfile exists. If so, this means we've just booted up
-  # a new Unicorn and need to tell the old one that it can now die. To do so
-  # we send it a QUIT.
-  #
-  # Using this method we get 0 downtime deploys.
-
-  old_pid = Rails.root + '/tmp/pids/unicorn.pid.oldbin'
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
