188857c2503e08670024e179eb51e2d679061aad
Scott Carlson <scarlson@inbox.com>
2013-01-09 10:11:22 -0700

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,9 +5,24 @@ worker_processes 3
 
 # Load your app into the master before forking
 # workers for super-fast worker spawn times
-preload_app false
+preload_app true
 
 # Immediately restart any workers that
 # haven't responded within 30 seconds
 timeout 30
 
+before_fork do |server, worker|
+  # Replace with MongoDB or whatever
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+end
+
+after_fork do |server, worker|
+  # Replace with MongoDB or whatever
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
+end
