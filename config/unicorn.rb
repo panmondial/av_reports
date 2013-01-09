d3386c7cf7e7febd2abed63be1e38d8a53dd2121
Scott Carlson <scarlson@inbox.com>
2013-01-09 09:56:53 -0700

    more unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,16 +12,28 @@ preload_app true
 timeout 30
 
 
-before_fork do |_server, _worker|
-  Signal.trap 'TERM' do
-    puts 'intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
 end
 
-# Fix PostgreSQL SSL error
-# http://stackoverflow.com/a/8513432/235297
-after_fork do |server, worker| 
-  defined?(ActiveRecord::Base) and 
-  ActiveRecord::Base.establish_connection 
+before_fork do |server, worker|
+  ##
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+
+  old_pid = Rails.root + '/tmp/pids/unicorn.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
 end
