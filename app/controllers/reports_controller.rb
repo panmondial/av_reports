aad724c6cde05b4f459b8cc19add1c6d66e74ff4
Scott Carlson <scarlson@inbox.com>
2013-01-11 09:24:41 -0700

    rolled back from thread testing

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,221 +1,103 @@
-require 'csv'
-require 'open-uri'
-require 'thread'
-
-
-class ReportsController < ApplicationController
-  before_filter :signed_in_user, except: :index
-  helper_method :build_pedigree
-  
-  def end_of_line
-	@filter_criteria = lambda { |ped| (ped.father_id.nil? || ped.mother_id.nil?) }
-	@report_title = __method__.to_s.titleize + " Report"
-	@report_description = "This report shows direct-line ancestors at the end of each branch of the family tree."
-	respond_to do |format|
-	  format.html {render '_report_detail', :layouts => 'reports', :locals => {:@filter_criteria => @filter_criteria } }
-	  format.csv do
-		build_csv
-        send_data @csv_array, filename: "#{__method__}.csv"
+module Org::Familysearch::Ws::Familytree::V2::Schema
+  class PedigreePerson < Person
+    attr_accessor :pedigree
+    
+    def initialize(pedigree = nil, person = nil)
+      if person
+        @id = person.id
+        # @version = person.version if person.version
+        @assertions = person.assertions if person.assertions
+        @families = person.families if person.families
+        @parents = person.parents if person.parents
+        @properties = person.properties if person.properties
       end
-	end
-  end
-
-  def direct_line_all
-	@filter_criteria = lambda { |ped| ped.id }
-	@report_title = __method__.to_s.titleize + " Report"
-	@report_description = "This report shows all ancestors in a direct-line relationship (ex: self, father, mother, grandfather, grandmother, etc.)"
-	respond_to do |format|
-	  format.html {render '_report_detail', :layouts => 'reports', :locals => {:@filter_criteria => @filter_criteria } }
-	  format.csv do
-		build_csv
-		send_data @csv_array, filename: "#{__method__}.csv"
-	  end
-	end
-  end
-
-  def missing_birth_date
-	@filter_criteria = lambda { |ped| !(ped.birth && ped.birth.date) }
-	@report_title = __method__.to_s.titleize + " Report"
-	@report_description = "This report shows all direct-line ancestors with no birth date,"
-	respond_to do |format|
-	  format.html {render '_report_detail', :layouts => 'reports', :locals => {:@filter_criteria => @filter_criteria } }
-	  format.csv do
-        build_csv	  
-	    send_data @csv_array, filename: "#{__method__}.csv"
-	  end
-	end
-  end
-
-  def missing_birth_place
-	@filter_criteria = lambda { |ped| !(ped.birth && ped.birth.place) }
-	@report_title = __method__.to_s.titleize + " Report"
-	@report_description = "This report shows all direct-line ancestors with no birth place."
-	@report_description = "This report shows a list of all direct-line ancestors with no birth date in the FamilySearch tree."
-	respond_to do |format|
-	  format.html {render '_report_detail', :layouts => 'reports', :locals => {:@filter_criteria => @filter_criteria } }
-	  format.csv do
-		build_csv
-		send_data @csv_array, filename: "#{__method__}.csv"
-	  end
-	end
-  end
-
-  def incomplete_birth_date
-	@filter_criteria = lambda { |ped| !((Date.strptime "#{ped.birth.date.normalized}", '%d %B %Y') rescue false) }
-	@report_description = "This report shows all direct-line ancestors with a partial or incomplete birth date."
-	@report_title = __method__.to_s.titleize + " Report"
-	respond_to do |format|
-	  format.html {render '_report_detail', :layouts => 'reports', :locals => {:@filter_criteria => @filter_criteria } }
-	  format.csv do
-		build_csv
-		send_data @csv_string, filename: "#{__method__}.csv"
-	  end
-	end
-  end
-	
-  def missing_death_date
-	@filter_criteria = lambda { |ped| !(ped.death && ped.death.date) }
-	@report_title = __method__.to_s.titleize + " Report"
-	@report_description = "This report shows all direct-line ancestors with no death date."
-	respond_to do |format|
-	  format.html {render '_report_detail', :layouts => 'reports', :locals => {:@filter_criteria => @filter_criteria } }
-	  format.csv do
-		build_csv
-        send_data @csv_array, filename: "#{__method__}.csv"
-	  end
-	end
-  end
-
-  def missing_death_place
-	@filter_criteria = lambda { |ped| !(ped.death && ped.death.place) }
-	@report_title = __method__.to_s.titleize + " Report"
-	@report_description = "This report shows all direct-line ancestors with no death place."
-	respond_to do |format|
-	  format.html {render '_report_detail', :layouts => 'reports', :locals => {:@filter_criteria => @filter_criteria } }
-	  format.csv do
-	    build_csv
-        send_data @csv_array, filename: "#{__method__}.csv"
+      if pedigree
+        @pedigree = pedigree
       end
-	end
-  end
-	
-  def incomplete_death_date
-	@filter_criteria = lambda { |ped| !((Date.strptime "#{ped.death.date.normalized}", '%d %B %Y') rescue false) }
-	@report_title = __method__.to_s.titleize + " Report"
-	@report_description = "This report shows all direct-line ancestors with a partial or incomplete death date."
-	respond_to do |format|
-	  format.html {render '_report_detail', :layouts => 'reports', :locals => {:@filter_criteria => @filter_criteria } }
-	  format.csv do
-	    build_csv
-		send_data @csv_array, filename: "#{__method__}.csv"
-	  end
-	end
-  end
-	
-  require 'ruby-fs-stack'
-  FamilyTreeV2 = Org::Familysearch::Ws::Familytree::V2::Schema
-
-    def build_pedigree
-	  subdomain = Rails.env.production? ? 'sandbox' : 'sandbox'
-	  #subdomain = Rails.env.production? ? 'api' : 'sandbox'
-	  @com = FsCommunicator.new(
-	    :domain => "https://#{subdomain}.familysearch.org",
-	    :handle_throttling => true,
-	    :session => session[:api_session_id]
-	  )
-
-	  # TODO: Check for session instead? Handle invalid session exception from ruby-fs-stack (401 Unauthorized)?
-	    @my_pedigree = @com.familytree_v2.pedigree :me
-		
-		
-	  # @my_pedigree = Rails.cache.fetch("fs_root_ids", expires_in: 5.minutes) do
-		# @com.familytree_v2.pedigree :me
-	  # end
-	  
-	  # @my_pedigree.continue_ids.each_slice(2) do |ids|
-	    # pedigrees = @com.familytree_v2.pedigree ids
-		# pedigrees.each do |ped|
-		  # @my_pedigree.injest ped
-		# end
-	  # end
-	  
-	  ped_queue = Queue.new
-	  semaphore_ped = Mutex.new
-	  
-	  @my_pedigree.continue_ids.each_slice(2) {|ids| ped_queue << ids }
-	  15.times.map {
-	    Thread.new do
-		  while !ped_queue.empty?
-		    ids = ped_queue.shift
-		    semaphore_ped.synchronize {
-			pedigrees = @com.familytree_v2.pedigree ids
-		    pedigrees.each do |ped|
-		      @my_pedigree.injest ped
-		    end
-			}
-		  end
-		end
-	  }.each {|thread| thread.join }
-	  	  
-	  # Rails.cache.fetch("fs_ids", expires_in: 5.minutes) do
-	    # @my_pedigree.continue_ids.each_slice(2) do |ids|
-	      # pedigrees = @com.familytree_v2.pedigree ids
-		  # pedigrees.each do |ped|
-		    # @my_pedigree.injest ped
-		  # end
-	    # end
-	  # end
-	  
-	  @pedigree = @my_pedigree
-      @full_pedigree = FamilyTreeV2::Pedigree.new
-	  ## @persons = @com.familytree_v2.person @pedigree.person_ids, :parents => 'all', :events=> 'standard', :names=> 'summary', :families=> 'summary'
-	  
-	  # @persons = Rails.cache.fetch("fs_detail", expires_in: 5.minutes) do
-	    # @com.familytree_v2.person @pedigree.person_ids, :parents => 'all', :events=> 'standard', :names=> 'summary', :families=> 'summary'
-	  # end
-	  
-	  # @persons.each do |person|
-  	    # @full_pedigree << person
-	  # end
-  
-  queue = Queue.new
-  ## semaphore = Mutex.new
-
-
-	  @pedigree.person_ids.each {|ids| queue << ids }
-	  15.times.map {
-	    Thread.new do
-		  while !queue.empty?
-		    ids = queue.pop
-			## semaphore.synchronize {
-            person = @com.familytree_v2.person ids#, :parents => 'all', :events=> 'standard', :names=> 'summary', :families=> 'summary'
-            @full_pedigree << person
-			## }
-		  end
-		end
-	  }.each {|thread| thread.join}
-  
-	  @pedigree = @full_pedigree
     end
-	
-  def build_csv
-	@csv_array = CSV.generate do |csv|
-      csv << ['FamilySearch ID', 'First Name', 'Last Name', 'Gender', 'Birth Date', 'Birth Place', 'Death Date', 'Death Place']
-      build_pedigree.persons.find_all(&@filter_criteria).each do |ped|
-        csv << [ped.id, \
-          (ped.assertions.names[0].value.forms[0].pieces.select {|fn| fn.type=="Given"}.collect {|fn| fn.value.to_s}.join(" ") \
-	        if ped.assertions && ped.assertions.names && ped.assertions.names[0].value && ped.assertions.names[0].value.forms[0] \
-	          && ped.assertions.names[0].value.forms[0].pieces[0]), \
-  	      (ped.assertions.names[0].value.forms[0].pieces.select {|fn| fn.type=="Family"}.collect {|fn| fn.value.to_s}.join(" ") \
-	        if ped.assertions && ped.assertions.names && ped.assertions.names[0].value && ped.assertions.names[0].value.forms[0] \
-	          && ped.assertions.names[0].value.forms[0].pieces[0]), \
-		  ped.gender, \
-		  (ped.birth.date.normalized if ped.birth && ped.birth.date && ped.birth.date.normalized), \
-		  (ped.birth.place.normalized.value if ped.birth && ped.birth.place && ped.birth.place.normalized && ped.birth.place.normalized.value), \
-		  (ped.death.date.normalized if ped.death && ped.death.date && ped.death.date.normalized), \
-		  (ped.death.place.normalized.value if ped.death && ped.death.place && ped.death.place.normalized && ped.death.place.normalized.value)] 
-      end
-    end 
+    
+    def father
+      pedigree.get_person(father_id)
+    end
+    
+    def mother
+      pedigree.get_person(mother_id)
+    end
+        
   end
-
+  
+  
+  
+  class Pedigree
+    attr_accessor :person_hash
+  
+    def initialize
+      @person_hash = {}
+      @persons = []
+    end
+  
+    def injest(pedigree)
+      @person_hash.merge!(pedigree.person_hash)
+      graft_persons_to_self(pedigree.persons)
+      @persons = @persons + pedigree.persons
+    end
+  
+    def <<(person)
+      p = PedigreePerson.new(self, person)
+      @persons << p
+      @person_hash[p.id] = p
+    end
+  
+    def continue_nodes
+      @persons.select do |person| 
+        (!person.mother_id.nil? && person.mother.nil?) || (!person.father_id.nil? && person.father.nil?)
+      end
+    end
+      
+    def continue_node_ids
+      continue_nodes.collect{|n|n.id}
+    end
+  
+    def continue_ids
+      cns = continue_nodes
+      father_ids = cns.select{|n|!n.father_id.nil?}.collect{|n|n.father_id}
+      mother_ids = cns.select{|n|!n.mother_id.nil?}.collect{|n|n.mother_id}
+      father_ids + mother_ids
+    end
+      
+    def get_person(id)
+      @person_hash[id]
+    end
+  
+    def person_ids
+      @persons.collect{|p|p.id}
+    end
+  
+    def init_jaxb_json_hash(_o)
+      @id = String.from_json(_o['id']) unless _o['id'].nil?
+      @requestedId = String.from_json(_o['requestedId']) unless _o['requestedId'].nil?
+      if !_o['persons'].nil?
+        @persons = Array.new
+        _oa = _o['persons']
+        _oa.each do | _item | 
+          pedigree_person = Org::Familysearch::Ws::Familytree::V2::Schema::PedigreePerson.from_json(_item)
+          pedigree_person.pedigree = self
+          @persons.push pedigree_person
+          @person_hash[pedigree_person.id] = pedigree_person
+        end
+      end
+    end
+  
+    def root
+      persons.first
+    end
+  
+    private
+    def graft_persons_to_self(persons_to_graft)
+      persons_to_graft.each do |person|
+        person.pedigree = self
+      end
+    end
+  
+  end
 end
