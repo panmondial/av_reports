3c01ca11359b863a060afd2b1380f5127129b778
Richard Hill <rhill@deseretbook.com>
2013-02-25 20:42:59 -0700

    Add path to ca_file for production env on Heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   if Rails.env.production?
     provider :familysearch, ENV['FAMILYSEARCH_DEVELOPER_KEY'], '',
-      :client_options => { :site => 'https://sandbox.familysearch.org' }
-    #provider :familysearch, ENV['FAMILYSEARCH_DEVELOPER_KEY'], '',
+      :client_options => {
+        :site => 'https://sandbox.familysearch.org', # TODO remove this line when we get a production developer key
+        :connection_opts => { :ssl => { :ca_file => '/usr/lib/ssl/certs/ca-certificates.crt' } }
+      }
   else
     provider :familysearch, ENV['FAMILYSEARCH_DEVELOPER_KEY'], '',
       :client_options => {
